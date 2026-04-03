6bdb6cca494c7943b9a729507551367a2af81699
Christian Evangelista <chriismatos@icloud.com>
2026-04-03 01:57:12 -0300

    frontend: 0.0.2

--- src/pages/Feedback/KioskPage.tsx
+++ src/pages/Feedback/KioskPage.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useMemo, useState } from "react";
-import { api } from "../services/api";
+import { api } from "../../services/api";
 
 type Step = "rating" | "tags" | "comment" | "done" | "error";
 
